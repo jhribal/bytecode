2f1260e0792ae58aa7c9e615825d5ab881f16060
jhribal <hribaljaromir@gmail.com>
2015-08-06 14:58:10 +0200

    init.sh update

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 git submodule init
 git submodule update
-git submodule foreach 'git fetch'
+git submodule foreach 'git fetch && git checkout master'
 
 gradle -b bcplugin/build.gradle getDeps
 gradle -b bclib/build.gradle getDeps
